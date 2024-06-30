69fc02e7b0e145b695e78619e995e1facd0ec890
SaiLord28 <nikomendi0728@gmail.com>
2024-06-29 21:21:02 -0500

    Second problem
    sum of vars

---
+++ Begginer/2) var.py
@@ -0,0 +1,28 @@
+# Sergio Nicolás Mendivelso Martinez <nikomendi0728@gmail.com>
+# 
+
+
+"""
+Second problem:
+
+Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.
+
+Input
+The input file will contain 2 integer numbers.
+
+Output
+Print the letter X (uppercase) with a blank space before and after the equal signal followed by the value of X, according to the following example.
+
+Obs.: don't forget the endline after all.
+
+"""
+
+#==================================== Solution =====================================
+
+A = input()
+B = input()
+
+X = int(A) + int(B)
+
+print(f"X = {X}")
+
