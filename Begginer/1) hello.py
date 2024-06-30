956ae8f9cc0ca6a139f5245e987c7d6a42bc9e73
SaiLord28 <nikomendi0728@gmail.com>
2024-06-29 21:03:40 -0500

    first problem
    writte hello world

---
+++ Begginer/1) hello.py
@@ -0,0 +1,18 @@
+# Sergio Nicolás Mendivelso Martinez <nikomendi0728@gmail.com>
+
+"""
+First problem:
+
+Your first program in any programming language is usually "Hello World!". In this first problem all you have to do is print this message on the screen.
+
+Input
+This problem has no input.
+
+Output
+You must print the message Hello World! and then the endline as shown below.
+
+"""
+
+#==================================== Solution =====================================
+
+print("Hello World!")
